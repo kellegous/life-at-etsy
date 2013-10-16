c76e03cd9d51ac17bc2d8b72831e059a5ea34c61
Kelly Norton <kellegous@gmail.com>
2013-10-16 06:32:52 -0400

    Moved raf into lib and updated jquery references

--- life.main.ts
+++ life.main.ts
@@ -1,5 +1,5 @@
-/// <reference path="jquery.d.ts" />
-/// <reference path="raf.ts" />
+/// <reference path="lib/jquery.d.ts" />
+/// <reference path="lib/raf.ts" />
 /// <reference path="lib/life.ts" />
 module life {
 
