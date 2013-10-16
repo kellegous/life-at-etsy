3016d1e218fad0dcbe23444dc0079fd1c9b28385
Kelly Norton <kellegous@gmail.com>
2013-10-16 09:34:04 -0400

    Drawing a single cube now.

--- 3d.main.ts
+++ 3d.main.ts
@@ -17,7 +17,7 @@ class View {
 
     // TODO: adjust camera position by aspect ratio
     var camera = new THREE.CombinedCamera(rect.width, rect.height, 40, 1, 10000, -2000, 10000);
-    camera.position.x = 0;
+    camera.position.x = 100;
     camera.position.y = 1000 * Math.sin(Math.PI / 4);
     camera.position.z = 1000 * Math.cos(Math.PI / 4);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
@@ -32,16 +32,49 @@ class View {
     plane.rotation.x = Math.PI / 2;
     scene.add(plane);
 
+    View.initLights(scene);
+    View.initCubes(scene);
+
     var renderer = new THREE.WebGLRenderer({ antialias : true });
     renderer.setSize(rect.width, rect.height);
     root.get(0).appendChild(renderer.domElement);
 
-    renderer.render(scene, camera);
+    setTimeout(() => {
+      renderer.render(scene, camera);
+    }, 1000);
+
 
     this.scene = scene;
     this.camera = camera;
     this.renderer = renderer;
   }
+
+  static initLights(scene : THREE.Scene) {
+    var ambient = new THREE.AmbientLight(0xff0000);
+    scene.add(ambient);
+
+    var directional = new THREE.DirectionalLight(0xffffff);
+    directional.position.set(1, 1, 1).normalize();
+    scene.add(directional);
+  }
+
+  static initCubes(scene : THREE.Scene) {
+    var geom = new THREE.CubeGeometry(20, 20, 20),
+        matr = new THREE.MeshLambertMaterial({
+          shading: THREE.SmoothShading,
+          map: THREE.ImageUtils.loadTexture('img/cube.png'),
+          color: 0xff9900,
+          ambient: 0xff9900
+        });
+
+
+    var cube = new THREE.Mesh(geom, matr);
+    cube.position.x = 0;
+    cube.position.y = 100;
+    cube.position.z = 0;
+    cube.visible = true;
+    scene.add(cube);
+  }  
 }
 
 var view = new View($('#view'));
